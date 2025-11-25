cef8864f4af79e8537752914fa32c9ccee72387e
hugopamponet <hugofarias9@gmail.com>
2025-11-25 14:00:57 -0300

    feat: create navigation routes for the menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Heading } from "./components/Heading";
 import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Competicoes } from "./components/Competicoes";
+import { Seminar } from "./components/Seminar";
+import { Courses } from "./components/Courses";
 
 export function App() {
   return (
     <>
-    <BrowserRouter>
-    <Heading />
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/competicoes" element={<Competicoes />} />
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Heading />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/competicoes" element={<Competicoes />} />
+          <Route path="/seminar" element={<Seminar />} />
+          <Route path="/courses" element={<Courses />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </>
   );
