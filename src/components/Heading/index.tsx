d50ff70c64c227b9a1a66cfb3b0450583b123528
hugopamponet <hugofarias9@gmail.com>
2025-11-29 22:53:55 -0300

    feat: create mobile hamburger menu

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,29 +1,69 @@
+import { useState } from "react";
 import styles from "./styles.module.css";
 import stylesButton from '../Button/styles.module.css'
-
 import { Button } from "../Button";
 import { Link } from "react-router-dom";
 
 export function Heading() {
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuAberto(!menuAberto);
+  };
+
+  const fecharMenu = () => {
+    setMenuAberto(false);
+  };
+
   return (
     <>
       <header>
         <div className={styles.menu}>
-          <Link to="/">
-            <img src="/images/logoHome.png" alt="Logo Publicando" className={styles.logoP} />
+          <Link to="/" onClick={fecharMenu}>
+            <img 
+              src="/images/logoHome.png" 
+              alt="Logo Publicando" 
+              className={styles.logoP} 
+            />
           </Link>
+          
           <h1 className={styles.title}>Jiu-Jitsu Eventos</h1>
-          <nav className={styles.navegacao}>
+          
+          {/* Botão Hambúrguer */}
+          <button 
+            className={styles.hamburger}
+            onClick={toggleMenu}
+            aria-label="Menu"
+          >
+            <span className={menuAberto ? styles.active : ''}></span>
+            <span className={menuAberto ? styles.active : ''}></span>
+            <span className={menuAberto ? styles.active : ''}></span>
+          </button>
+
+          {/* Menu de Navegação */}
+          <nav className={`${styles.navegacao} ${menuAberto ? styles.navAberta : ''}`}>
             <ul>
-              <li><Link to="/"> Inicio</Link></li>
-              <li><Link to="/competicoes">Competições</Link></li>
-              <li><Link to="/seminar">Seminários</Link></li>
-              <li><Link to="/courses">Cursos</Link></li>
-              <Link to="/criarConta"><Button children="Criar conta" className={stylesButton.botaoVermelho}/></Link>
-              <Link to="/login"><Button children="Login" className={stylesButton.botaoVermelho}/></Link>
+              <li><Link to="/" onClick={fecharMenu}>Inicio</Link></li>
+              <li><Link to="/competicoes" onClick={fecharMenu}>Competições</Link></li>
+              <li><Link to="/seminar" onClick={fecharMenu}>Seminários</Link></li>
+              <li><Link to="/courses" onClick={fecharMenu}>Cursos</Link></li>
+              <Link to="/criarConta" onClick={fecharMenu}>
+                <Button children="Criar conta" className={stylesButton.botaoVermelho}/>
+              </Link>
+              <Link to="/login" onClick={fecharMenu}>
+                <Button children="Login" className={stylesButton.botaoVermelho}/>
+              </Link>
             </ul>
           </nav>
         </div>
+        
+        {/* Overlay quando menu está aberto */}
+        {menuAberto && (
+          <div 
+            className={styles.overlay} 
+            onClick={fecharMenu}
+          ></div>
+        )}
       </header>
     </>
   );
